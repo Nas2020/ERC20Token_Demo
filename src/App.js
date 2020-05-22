c49b619f62447332f5bbe316396222b9f6abe98b
nlmuasys <nlmuasys@gmail.com>
2020-05-22 15:49:08 -0400

    featureA js files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Drizzle } from '@drizzle/store';
+import { drizzleReactHooks } from "@drizzle/react-plugin";
+
+import drizzleOptions from "./drizzleOptions";
+import LoadingContainer from './LoadingContainer.js';
+import TokenMetadata from './TokenMetadata.js';
+import TokenWallet from './TokenWallet.js';
+
+const drizzle = new Drizzle(drizzleOptions);
+const { DrizzleProvider } = drizzleReactHooks;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>ERC20 Token</h1>
+      <DrizzleProvider drizzle={drizzle}>
+        <LoadingContainer>
+          <TokenMetadata />
+          <TokenWallet />
+        </LoadingContainer>
+      </DrizzleProvider>
     </div>
   );
 }
 
 export default App;
-
